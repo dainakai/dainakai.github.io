4cccf9131254ec4cba9ea1a45992f4b0d4b41cae
dainakai <dainakai1031@icloud.com>
2023-05-16 16:35:22 +0900

    update

--- src/components/contactme.js
+++ src/components/contactme.js
@@ -2,7 +2,7 @@ export const ContactMe = () => {
     return (
         <div className="contactme">
             <h2 className='contactme_title'>Contact</h2>
-            <p className='contactme_text'>If you want to contact me, please email at</p>
+            <p className='contactme_text'>If you want to contact me, please email at <a href="mailto:m2623119@edu.kit.ac.jp">m2623119 at edu.kit.ac.jp</a> (please replace 'at' with '@')</p>
         </div>
     );
 }
